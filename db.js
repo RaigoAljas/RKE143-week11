8fafe84d647f1fd0d3e202c289a99e4cabb29200
Raigo Aljas <raigo.aljas@tktk.ee>
2025-12-15 23:02:23 +0200

    week 11 dotenv

---
+++ db.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { Pool } = require('pg');
+
+const itemsPool = new Pool ({
+    connectionString: process.env.DBConnectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+
+
+
+
+module.exports = itemsPool;
